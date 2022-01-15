fd7bbea841e89ad0e9afcccb399ad2554cdf3161
Andrei Chirila <andrei_chirila@outlook.com>
2022-01-15 14:41:48 +0200

    feat: add biolnk to projects list (#5)
    * chore: update assets for main projects
    
    * feat: add biolnk project to the data && changed the preview images
    
    * chore: upgrade next to ver.12 && bumped pckg ver

--- data/home/projects.ts
+++ data/home/projects.ts
@@ -10,12 +10,21 @@ export interface IProjectData {
 
 const projectData: IProjectData[] = [
   {
+    title: 'Biolnk',
+    scope: 'UX/UI Design. Web App Development',
+    tags: ['nextjs', 'tailwindcss', 'supabase', 'react-query'],
+    summary:
+      'Combine all your online content into one short, easy Biolink. You can also create Biolink pages for things like blogs, merch, multimedia and so much more.',
+    srcName: 'biolnk-preview.png',
+    link: 'https://biolnk.me/',
+  },
+  {
     title: 'Margatsni',
     scope: 'Web App Development',
     tags: ['react', 'tailwindcss', 'firebase', 'cloudinary'],
     summary:
       'Margatsni is an unofficial web application build of the famous Instagram by Facebook, providing a full social media experience in the browser.',
-    srcName: 'margatsni-mock.png',
+    srcName: 'margatsni-preview.png',
     link: '/writing/margatsni',
   },
   {
@@ -24,7 +33,7 @@ const projectData: IProjectData[] = [
     tags: ['gatsbyjs', 'styled-components', 'typescript'],
     summary:
       'After some independent work I decided to form a centralized place which will facilitate the working process with local clients from early consulting and design stages all the way up to development and marketing of their products and ideas.',
-    srcName: 'nectbox-mock.png',
+    srcName: 'nectbox-preview.png',
     link: '/writing/nectbox',
   },
   {
@@ -33,7 +42,7 @@ const projectData: IProjectData[] = [
     tags: ['nextjs', 'tailwindcss', 'supabase'],
     summary:
       "Montex it's the easiest way to add comments or reviews to your blog or personal website. Implementing a feedback system into your website was never that easy.",
-    srcName: 'montex-mock.png',
+    srcName: 'montex-preview.png',
     link: '/writing/montex',
   },
 ];
